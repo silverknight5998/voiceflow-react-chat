d4a253c68249f2f776f6bd15d9096dd2fec18419
silvergoldperson <silverknight5998@gmail.com>
2023-11-22 16:45:20 -0500

    s3 upload

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -14,7 +14,7 @@ import { DemoContainer } from './styled';
 import { useLiveAgent } from './use-live-agent.hook';
 const AWS = require('aws-sdk');
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
+  accessKeyId: process.env.AWS_KEY,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
 });
