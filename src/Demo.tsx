2bd618860c110094be0ea635ef68eb5c5dc73daa
silvergoldperson <silverknight5998@gmail.com>
2023-11-23 12:11:05 -0500

    fix

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,5 +1,5 @@
 import 'react-calendar/dist/Calendar.css';
-import './app.css';
+
 import { Chat, ChatWindow, Launcher, RuntimeAPIProvider, SessionStatus, SystemResponse, TurnType, UserResponse, Button } from '@voiceflow/react-chat';
 import { useContext, useState } from 'react';
 import { match } from 'ts-pattern';
